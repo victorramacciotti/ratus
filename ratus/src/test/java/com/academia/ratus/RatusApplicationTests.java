5ebc4dd01277ac107f271db0b56774bc47d8b8fb
William Almeida <almeida.william0208@gmail.com>
2025-07-02 11:05:37 -0300

    codes created from spring initializr

---
+++ ratus/src/test/java/com/academia/ratus/RatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academia.ratus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
